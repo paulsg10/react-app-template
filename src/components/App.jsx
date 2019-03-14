116cc355b2d0ddfc6d44679573fba478111073a0
Paul Gi <paulsgi@PAUL-MAC.local>
2019-03-14 10:10:53 -0700

    single quote to double quote

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import '../styles/App.css';
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <h1>Hello, World!</h1>
       </div>
     );
